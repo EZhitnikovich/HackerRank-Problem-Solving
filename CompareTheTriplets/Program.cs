83f6ea011c718ab2e66e47cca2b3cb21b6420482
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-04 22:23:17 +0300

    add "compare the triplets" task

---
+++ CompareTheTriplets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static List<int> CompareTriplets(List<int> first, List<int> second)
+        {
+            if (first.Count != second.Count)
+                throw new Exception("Different length");
+
+            List<int> result = new List<int>() { 0, 0 };
+
+            for(int i = 0; i < second.Count; i++)
+            {
+                if(first[i] > second[i])
+                {
+                    result[0]++;
+                }
+                else if(first[i] < second[i])
+                {
+                    result[1]++;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> a = InputIntList();
+            List<int> b = InputIntList();
+
+            Console.WriteLine(String.Join(" ", CompareTriplets(a,b)));
+        }
+
+        static List<int> InputIntList()
+            => Console.ReadLine().Split(' ').Select(temp => Convert.ToInt32(temp)).ToList();
+    }
+}
