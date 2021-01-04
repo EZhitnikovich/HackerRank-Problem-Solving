d50a984b0967cc32b0ad4ae62d35f0be0ec3eda5
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-04 21:14:40 +0300

    Add project files.

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(int[] ar) => ar.Sum();
+
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = simpleArraySum(ar[0..len]);
+
+            Console.WriteLine(result);
+        }
+    }
+}
