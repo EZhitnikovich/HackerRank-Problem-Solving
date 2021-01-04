542f3f7ae7ccf021818b5653a0e948d57a62841f
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-04 22:34:13 +0300

    add "A Very Big Sum" task

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static long AVeryBigSum(long[] ar)
+            => ar.Sum();
+
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), temp => long.Parse(temp));
+
+            long result = AVeryBigSum(ar[0..len]);
+
+            Console.WriteLine(result);
+        }
+    }
+}
