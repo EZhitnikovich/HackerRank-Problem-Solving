95a57582fe6589c1027e00ea8adbd6a572e31961
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 22:27:10 +0300

    add "Time Conversion" task

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time12 = Console.ReadLine();
+            Console.WriteLine(ConvertTo24Format(time12));
+        }
+
+        static string ConvertTo24Format(string time12)
+        {
+            string time24 = string.Empty;
+            if (time12.Contains("AM"))
+            {
+                var timeElements = time12.Replace("AM", "").Split(':');
+                if (timeElements[0].Equals("12"))
+                {
+                    timeElements[0] = "00";
+                }
+                time24 = $"{timeElements[0]}:{timeElements[1]}:{timeElements[2]}";
+            }
+            else if (time12.Contains("PM"))
+            {
+                var timeElements = time12.Replace("PM", "").Split(':');
+                if (!timeElements[0].Equals("12"))
+                {
+                    timeElements[0] = Convert.ToString(int.Parse(timeElements[0]) + 12);
+                }
+                time24 = $"{timeElements[0]}:{timeElements[1]}:{timeElements[2]}";
+            }
+
+            return time24;
+        }
+    }
+}
