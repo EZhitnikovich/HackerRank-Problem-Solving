4f53cd02bd4cb6699bf53fa86ced6ca607d515af
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 19:50:36 +0300

    add "Birthday Cake Candles"

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = InputIntList();
+            Console.WriteLine(CountMax(number));
+        }
+
+        static int CountMax(IEnumerable<int> number)
+            => number.Count(x=>x==number.Max());
+
+        static List<int> InputIntList()
+            => Console.ReadLine().Split(' ').Select(temp => Convert.ToInt32(temp)).ToList();
+    }
+}
