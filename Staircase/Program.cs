b9ac36d40b71c16a00d4ae0ae6717c8074f31427
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 18:23:37 +0300

    add "Staircase" task

---
+++ Staircase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            char rungSymbol = '#';
+            char surroundingSymbol = ' ';
+            Console.WriteLine(MakeStaircase(len, rungSymbol, surroundingSymbol));
+        }
+
+        static string MakeStaircase(int len, char rungSymbol, char surroundingSymbol)
+        {
+            string staircase = string.Empty;
+            for (int i = 1; i <= len; i++)
+            {
+                staircase += "\n".PadLeft(i + 1, rungSymbol).PadLeft(len + 1, surroundingSymbol);
+            }
+
+            return staircase;
+        }
+    }
+}
