924df6e0c3444bff6f0dd99383e58f245eee910b
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 20:32:26 +0300

    minor changes

--- BirthdayCakeCandles/Program.cs
+++ BirthdayCakeCandles/Program.cs
@@ -8,12 +8,29 @@ namespace BirthdayCakeCandles
     {
         static void Main(string[] args)
         {
+            var len = int.Parse(Console.ReadLine());
             var number = InputIntList();
-            Console.WriteLine(CountMax(number));
+            Console.WriteLine(CountMax(number.GetRange(0, len)));
         }
 
-        static int CountMax(IEnumerable<int> number)
-            => number.Count(x=>x==number.Max());
+        static long CountMax(IEnumerable<int> number)
+        {
+            int maxValue = number.First();
+            int count = 0;
+            foreach (int item in number)
+            {
+                if (item > maxValue)
+                {
+                    maxValue = item;
+                    count = 1;
+                }
+                else if(item == maxValue)
+                {
+                    count++;
+                }
+            }
+            return count;
+        } 
 
         static List<int> InputIntList()
             => Console.ReadLine().Split(' ').Select(temp => Convert.ToInt32(temp)).ToList();
