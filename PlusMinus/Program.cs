496a8c06e417fc5c4a16b2d43f6ce8d5d50784db
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-05 17:25:56 +0300

    add "Plus Minus" task

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            List<int> numbers = InputIntList().GetRange(0, len);
+
+            var pZero = GetPercentOfZero(numbers);
+            var pPositive = GetPercentOfPositive(numbers);
+            var pNegative = GetPercentOfNegative(numbers);
+
+            Console.WriteLine(string.Format("{0:f6}\n" +
+                                            "{1:f6}\n" +
+                                            "{2:f6}",
+                                            pPositive,
+                                            pNegative,
+                                            pZero));
+        }
+
+        static double GetPercentOfZero(List<int> numbers)
+            => numbers.Where(x => x == 0).Count() / (double)numbers.Count;
+
+        static double GetPercentOfPositive(List<int> numbers)
+            => numbers.Where(x => x > 0).Count() / (double)numbers.Count;
+
+        static double GetPercentOfNegative(List<int> numbers)
+            => numbers.Where(x => x < 0).Count() / (double)numbers.Count;
+
+        static List<int> InputIntList()
+            => Console.ReadLine().Split(' ').Select(temp => Convert.ToInt32(temp)).ToList();
+    }
+}
