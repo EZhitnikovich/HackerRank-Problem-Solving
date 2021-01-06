0c7141801d78a295e2e0796c31a42b948dc83b97
EZhitnikovich <ezhitnikovich@gmail.com>
2021-01-06 23:53:09 +0300

    start work on the task "Number Line Jumps"

---
+++ NumberLineJumps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace NumberLineJumps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+            int x1 = Convert.ToInt32(x1V1X2V2[0]);
+            int v1 = Convert.ToInt32(x1V1X2V2[1]);
+            int x2 = Convert.ToInt32(x1V1X2V2[2]);
+            int v2 = Convert.ToInt32(x1V1X2V2[3]);
+        }
+    }
+}
