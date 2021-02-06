643eea0dd2d2fa4534a2d74cc523e08e96b9aacc
EZhitnikovich <ezhitnikovich@gmail.com>
2021-02-06 21:24:23 +0300

    add Day of the Programmer task

---
+++ DayOfTheProgrammer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DayOfTheProgrammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            string result = DayOfProgrammer(year);
+            Console.WriteLine(result);
+        }
+
+        static string DayOfProgrammer(int year)
+        {
+            if(year%4==0 && (year<1918 || year %400==0 || year % 100 != 0))
+            {
+                return $"12.09.{year}";
+            }
+            else if(year == 1918)
+            {
+                return $"26.09.{year}";
+            }
+            else
+            {
+                return $"13.09.{year}";
+            }
+        }
+    }
+}
